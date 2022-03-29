d70b427b076c2e697c80f8cc8df84e1fcbfe312e
daniMcReynolds <72697955+daniMcReynolds@users.noreply.github.com>
2022-03-29 16:52:44 -0400

    Add files via upload

---
+++ app/buzzer.py
@@ -0,0 +1,13 @@
+import socket
+
+
+
+HOST = "10.20.0.197"
+PORT = 6668
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST,PORT))
+    s.sendall(b'{"Tweet Type":"Service call","Thing ID":"G1_thing","Space ID":"G1_smart_space","Service Name":"pulse_buzzer","Service Inputs":"(1000)"}')
+    data = s.recv(1024)
+
+print(f"Received {data!r}")
