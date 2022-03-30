ecbf832ca30439d42eec47a5c89ebd9d8c31f217
a-padilla <78828245+a-padilla@users.noreply.github.com>
2022-03-29 22:47:11 -0400

    Update buzzer.cpp

--- thing_services/buzzer/buzzer.cpp
+++ thing_services/buzzer/buzzer.cpp
@@ -3,18 +3,18 @@
 
 #define BUZZ_PIN 23
 
-void pulse_buzz() {
+void pulse_buzz(int time) {
     wiringPiSetupGpio();
     pinMode(BUZZ_PIN, OUTPUT);
     
     digitalWrite(BUZZ_PIN, HIGH);
-    delay(2000);
+    delay(time);
     digitalWrite(BUZZ_PIN, LOW);
     
 }
 
 int main() {
-	pulse_buzz();
+	pulse_buzz(2000);
 	delay(1000);
-	pulse_buzz();
+	pulse_buzz(2000);
 }
